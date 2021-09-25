9a31674c44c66ee523c0918760ab7ea631a0e9cb
dizsel74 <arturomendozarendon@gmail.com>
2021-09-24 22:39:55 -0500

    Validar insert y fopen para corroborar pasos

--- BLOB_Servidor/datosBLOB.php
+++ BLOB_Servidor/datosBLOB.php
@@ -7,35 +7,44 @@ if(isset($_POST["cargar"])){    //RECIBIR DATOS IMAGEN
         $tipo_archivo=$_FILES["archivo"]["type"];
         $tamano_archivo=$_FILES["archivo"]["size"];
 
-        if($tamano_archivo < 200000){ //VERIFICA SI ES EL TAMAÑO
+        if($tamano_archivo < 500000){ //VERIFICA SI ES EL TAMAÑO  5kb
 
-            require_once("conectar.php");
+            require("conectar.php");
             $conexion_BD_insert = Conectar::conexion(); 
 
-           $temporal=$_FILES["archivo"]["tmp_name"]; //RUTA DE TEMPORAL EN PC
-           $carpeta_destino=$_SERVER["DOCUMENT_ROOT"]."/archivosBLOB/"; //RUTA CARPETA DESTINO EN SERVIDOR www o httpdocs
-            move_uploaded_file($temporal,$carpeta_destino.$nom_archivo);// MOVER DE TEMPORAL EN PC A CARPETA DEL SERVIDOR
-           
-        $archivo_aInsertar = fopen($carpeta_destino.$nom_archivo, "r");
-        $archivo_enBLOB = fread($archivo_aInsertar,$tamano_archivo);
-        fclose($archivo_aInsertar);
-
-          
+            $temporal=$_FILES["archivo"]["tmp_name"]; //RUTA DE TEMPORAL EN PC
+            $carpeta_destino=$_SERVER["DOCUMENT_ROOT"]."/archivosBLOB/"; //RUTA CARPETA DESTINO EN SERVIDOR www o httpdocs
             
-            //$archivo_insertar = $carpeta_destino.$nom_archivo; toda la ruta desde c:
-            //$archivo_insertar = "/archivosBLOB/".$nom_archivo; // solo la ruta para cargar la imagen  despues en una web
+            $carpeta_destino=addslashes($carpeta_destino);
 
-            $query = "INSERT INTO archivosblob ( nombre, tipo, contenido ) VALUES (:NOM, :TIPO, :CONT)"; //CONSULTAS PREPARADAS
-            
+            $archivo_aInsertar = fopen($carpeta_destino.$nom_archivo, "r"); //CREA LA RUTA DEL ARCHIVO A INSERTA
+
+            if($archivo_aInsertar != NULL){//VERIFICA QUE SE HALLA CREADO LA RUTA
+
+                $archivo_enBLOB = fread($archivo_aInsertar,$tamano_archivo);
+                
+                $query = "INSERT INTO archivosblob ( nombre, tipo, contenido ) VALUES (:NOM, :TIPO, :CONT)"; //CONSULTAS PREPARADAS
                 $resultado = $conexion_BD_insert->prepare($query);
-    
                 $resultado->execute(array(":NOM" => $nom_archivo, ":TIPO"=> $tipo_archivo, ":CONT"=>$archivo_enBLOB));// $resultado->execute(array(":FOTO" => $archivo_insertar));
-    
-                $resultado->closeCursor();
+               
 
-                header("Location:index.php"); // REGRESA AL TERMINAR CARGA
+                if($resultado->rowCount()> 0){ //VERIFICA SI SE INSERTO A LA BBDD
+                    $resultado->closeCursor();
+                    move_uploaded_file($temporal,$carpeta_destino.$nom_archivo);// MOVER DE TEMPORAL EN PC A CARPETA DEL SERVIDOR
+                    echo '<script type="text/javascript">alert("Archivo Incuido");</script>';
+
+                }else{
+                    echo "ERROR". $e->getLine();
+                }
+                fclose($archivo_aInsertar);
+                //header("Location:index.php"); // REGRESA AL TERMINAR CARGA
+                           
+            }else{
+                echo '<script type="text/javascript">alert("No Se pudo abrir fopen -> Archivo NO Incuido");</script>';
+            }
         }else{
-            header("Location:index.php"); // REGRESA SI EL ARCHIVO ES MUY GRANDE
+           // header("Location:index.php"); // REGRESA SI EL ARCHIVO ES MUY GRANDE
+           echo '<script type="text/javascript">alert("Archivo Muy Grande");</script>';
         }       
     }else{
         header("Location:index.php"); // REGRESA SI ESTA VACIO EN ENVIO
